9b9667077c3872b4baa47ae201ea657290be17b2
unknown <john.ong135@gmail.com>
2020-03-30 21:47:22 -0700

    Updated the comments on current App.js

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ const App = () => {
   const APP_ID = '975eb510';
   const APP_KEY = '3fd5ce701d54aca41a85f9e7170dc191';
 
-
-  const [recipes, setRecipes] = useState([]);
-  const [search, setSearch] = useState("");
-  const [query, setQuery] = useState('chicken');
+  
+  const [recipes, setRecipes] = useState([]);//This state will hold the array of recipe objects retrieved from the query
+  const [search, setSearch] = useState(""); //This is the state for the search bar
+  const [query, setQuery] = useState('chicken'); //This is the default state for the search
 
   //This will generate the actual queries and return JSON formatted response. The recipes state will then be set to an array of JSON objects
   const getRecipes = async () => {
@@ -48,7 +48,12 @@ const App = () => {
       </form>
       <div className="recipes">
       {recipes.map(recipe => (
-      <Recipe key={recipe.recipe.label} title={recipe.recipe.label} calories={recipe.recipe.calories} image={recipe.recipe.image} ingredients={recipe.recipe.ingredients}/>))}
+      <Recipe 
+        key={recipe.recipe.label} 
+        title={recipe.recipe.label} 
+        calories={recipe.recipe.calories} 
+        image={recipe.recipe.image} 
+        ingredients={recipe.recipe.ingredients}/>))}
       </div>
     </div>
   );
